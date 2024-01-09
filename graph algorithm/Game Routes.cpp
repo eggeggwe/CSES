9c570d04482b3381da69f4454c5d449c344521ff
eggeggwe <westinlai940221@gmail.com>
2024-01-09 23:53:50 +0800

    graph game routes bug fix

--- graph algorithm/Game Routes.cpp
+++ graph algorithm/Game Routes.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 vector<int> graph[100010];
 long long int dp[100010];
+bitset<100010> visited;
 void dfs(int a){
+    visited[a]=1;
     for(int i=0;i<graph[a].size();i++){
         int pre=graph[a][i];
-        if(dp[pre]==0)dfs(pre);
+        if(!visited[pre])dfs(pre);
         dp[a]=(dp[a]+dp[pre])%1000000007;
     }
     return;
 }
 int main() {
+    visited.reset();
     // ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
     int n,m;
     cin>>n>>m;
